e4d62f827a36af09f9187b00bd8ae667ac894fc4
Kristyvdm2020 <kristyvdm2020@gmail.com>
2023-01-05 10:52:14 -0500

    added postBank, modified app, installed npm

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const postBank = require('./postBank');
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  //TODO - get all the posts
+  const posts = postBank.list();
+  // console.log(posts);
+  res.send(`
+    <html>
+      <head>
+        <link rel='stylesheet' href='/style.css' />
+        <title>Wizard News</title>
+      </head>
+      <body>
+        <h1><img src='/logo.png'/>Wizard News</h1>
+        <ul>
+          ${posts
+            .map((post) => {
+              return `<li>${post.title}</li>`;
+            })
+            .join('')}
+        </ul>
+      </body>
+    </html>
+  `);
+});
 
 const PORT = 1337;
 
